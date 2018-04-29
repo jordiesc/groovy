2f9e1af39d6e818562332f138cf9113069d30905
jordi <jordi.escofet@seat.es>
2018-04-29 08:12:02 +0200

    create basic urber jar for goovy projercts

---
+++ groovyurberjar/src/main/groovy/com/test/Example.groovy
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Example {
+   public static void main(String ... args ){
+      // Using a simple println statement to print output to the console
+      println('Hola mundo')
+
+      Class c = this.getClass()
+      println("el paquete es: "+c.getPackage())
+      println("la clase es:"+ c.getSimpleName())
+   }
+}
